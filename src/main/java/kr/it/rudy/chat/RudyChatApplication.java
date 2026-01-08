32eefe944402dff0cfedb8f9dfe56a4e1520263a
Rudy <idejrdud@gmail.com>
2026-01-08 15:40:21 +0900

    first commit

---
+++ src/main/java/kr/it/rudy/chat/RudyChatApplication.java
@@ -0,0 +1,13 @@
+package kr.it.rudy.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RudyChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RudyChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/it/rudy/chat/RudyChatApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.it.rudy.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RudyChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
